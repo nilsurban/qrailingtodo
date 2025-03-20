2e4cb390085fcdd19ea09a1c1870eb167846aac7
Nils Urban <urban.nils@web.de>
2025-03-20 17:17:54 +0100

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
@@ -174,5 +175,31 @@ const App = () => {
   );
   //#endregion
 };
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+>>>>>>> c9c371c (Initialize project using Create React App)
 
 export default App;
